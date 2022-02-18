dce946c3deeb7f55e30a8c6e3fd6421b4dbb250d
fairay <ffairay@gmail.com>
2022-02-18 12:45:31 +0300

    added super_new_function

--- main.c
+++ main.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+int super_new_function() {
+	return 42;
+}
+
 int main() {
 	
-	printf("The main thing in programming is ...");
+	printf("The main thing in programming is amount of code");
 	return 0;
 }
